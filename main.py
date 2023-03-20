285b394150a4154adff6d77bf997b63d3411280b
Geodeziy <geodez_y@bk.ru>
2023-03-20 08:58:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from data import db_session
+from data.users import *
+from data.jobs import *
+from flask import render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+def non_index():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/jobs')
+def main():
+    db_session.global_init('db/blogs.sqlite')
+    session = db_session.create_session()
+    jobs = session.query(Jobs).all()
+    users = session.query(User).all()
+    return render_template('jobs.html', jobs=jobs, users=users)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
